5b2029b15762c01301f453c8634abcbdef7cc1b7
Rideen <narancic.rideen@gmail.com>
2021-01-20 20:45:57 +0100

    Static File Middleware implementation

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { Application, Router, Context } from "https://deno.land/x/oak@v6.4.1/mod.ts";
+export { Application, Router, Context, send } from "https://deno.land/x/oak@v6.4.1/mod.ts";
 export type { RouterContext } from "https://deno.land/x/oak@v6.4.1/mod.ts";
 export { init, MongoClient } from "https://deno.land/x/mongo@v0.12.1/mod.ts";
 export { hashSync, compareSync } from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { Application } from './deps.ts';
+import { staticFileMiddleware } from "./middlewares/staticFileMiddleware.ts";
 import router from './router.ts';
 
 //  denon run --allow-net --allow-read --allow-write --allow-env --allow-plugin --unstable server.ts
@@ -7,6 +8,7 @@ const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods()); 
+app.use(staticFileMiddleware);
 
 app.addEventListener('listen', ({ hostname, port, secure }) => {
   console.log(`Listening on ${secure ? 'https://' : 'http://'}${hostname || "localhost"}:${port}`)
